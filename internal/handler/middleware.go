e7572b83cd218de27f2d5cb018549bdafe1be96c
achieva.gemilang <achievafuturagemilang@gmail.com>
2026-01-30 14:57:41 +0700

    fix: fix cors

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -142,8 +142,8 @@ func RecoveryMiddleware(next http.Handler) http.Handler {
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
